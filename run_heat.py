385ecc35d76a4ade51217b712bb8ba8def5cd775
mcflugen <mcflugen@gmail.com>
2016-05-04 14:24:16 -0600

    Cleaned up the time loop.

--- run_heat.py
+++ run_heat.py
@@ -12,31 +12,30 @@ GRID_ORIGIN = (0., 0.)
 ALPHA = 1.
 N_STEPS = 10
 
-time = 0.
-time_step = min(GRID_SPACING) ** 2 / (4. * ALPHA)
-temperature = random.random(GRID_SHAPE)
-next_temperature = np.empty_like(temperature)
+TIME_STEP = min(GRID_SPACING) ** 2 / (4. * ALPHA)
+DY2, DX2 = GRID_SPACING[0] ** 2, GRID_SPACING[1] ** 2
+STENCIL = np.array([[0., DY2, 0.],
+                    [DX2, -2. * (DY2 + DY2), DX2],
+                    [0., DY2, 0.]]) * ALPHA * TIME_STEP / (DX2 * DY2)
 
-print('Using dt = {dt}'.format(dt=time_step))
+time = 0.
+temperature = random.random(GRID_SHAPE)
+delta_temperature = np.empty_like(temperature)
+
+print('Using dt = {dt}'.format(dt=TIME_STEP))
 
 for step in xrange(N_STEPS):
     if step % 5 == 0:
         print('Step = {step}'.format(step=step))
         np.savetxt(sys.stdout, temperature, fmt='%6.3G')
 
-    dy2, dx2 = GRID_SPACING[0] ** 2, GRID_SPACING[1] ** 2
-    stencil = np.array([[0., dy2, 0.],
-                        [dx2, -2. * (dx2 + dy2), dx2],
-                        [0., dy2, 0.]]) * ALPHA * time_step / (dx2 * dy2)
+    ndimage.convolve(temperature, STENCIL, output=delta_temperature)
+    delta_temperature[(0, -1), :] = 0.
+    delta_temperature[:, (0, -1)] = 0.
 
-    ndimage.convolve(temperature, stencil, output=next_temperature)
-    next_temperature[(0, -1), :] = 0.
-    next_temperature[:, (0, -1)] = 0.
-    next_temperature += temperature
+    temperature += delta_temperature
 
-    np.copyto(temperature, next_temperature)
-
-    time += time_step
+    time += TIME_STEP
 
 print('Step = {step}'.format(step=step))
 np.savetxt(sys.stdout, temperature, fmt='%6.3G')
