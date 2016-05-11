6336259167b6bf38e34613bc316081ea9bda5b63
mcflugen <mcflugen@gmail.com>
2016-05-11 12:43:01 -0600

    Added files for step-c.

---
+++ heat_solver/heat.py
@@ -0,0 +1,73 @@
+#! /usr/bin/env python
+import warnings
+
+import numpy as np
+
+from .solve_2d import solve_2d
+
+
+class HeatSolver(object):
+
+    def __init__(self, shape=(10, 10), spacing=(1., 1.), alpha=1.):
+        """Solve the heat equation on a rectilinear grid.
+
+        Parameters
+        ----------
+        shape : tuple of int, optional
+            Number of rows and columns for the solution grid.
+        spacing : tuple of float, optional
+            Spacing between grid rows and columns.
+        alpha : float, optional
+            Thermal conductivity.
+        """
+        self._shape = tuple(shape)
+        self._spacing = tuple(spacing)
+        self._alpha = float(alpha)
+
+        self._time_step = min(self.spacing) ** 2 / (4. * self.alpha)
+        self._time_step /= 2.
+
+        self._temperature = np.zeros(self.shape)
+        self._delta_temperature = np.empty_like(self.temperature)
+
+    @property
+    def time_step(self):
+        """The time step."""
+        return self._time_step
+
+    @time_step.setter
+    def time_step(self, new_dt):
+        if new_dt >= min(self.spacing) ** 2 / (4. * self.alpha):
+            warnings.warn('using an unstable time step')
+        self._time_step = new_dt
+
+    @property
+    def shape(self):
+        """Number of grid rows and columns."""
+        return self._shape
+
+    @property
+    def spacing(self):
+        """Spacing between grid rows and columns."""
+        return self._spacing
+
+    @property
+    def alpha(self):
+        """Thermal conductivity."""
+        return self._alpha
+
+    @property
+    def temperature(self):
+        """Temperature values on the grid."""
+        return self._temperature
+
+    @temperature.setter
+    def temperature(self, new_temp):
+        self._temperature[:] = new_temp
+
+    def solve(self):
+        """Solve for the new temperatures."""
+        solve_2d(self.temperature, spacing=self.spacing,
+                 alpha=self.alpha, time_step=self.time_step,
+                 out=self._delta_temperature)
+        self._temperature += self._delta_temperature
---
+++ run_heat.py
@@ -0,0 +1,49 @@
+"""Solve the heat equation of a uniform rectilinear grid.
+
+Solve the heat equation of a rectilinear grid with shape (*GRID_NY*,
+*GRID_NX*) and row and column spacing (*GRID_DY*, *GRID_DX*). The thermal
+conductivity is given by *ALPHA*.
+
+In cartesian coordinates, the heat equation is the following parabolic
+differential equation,
+
+    del u / del t - alpha (del^2 u / del x^2 + del^2 u / del y^2) = 0
+
+This becomes the following finite-difference problem,
+
+    Delta u = alpha Delta t ((u_i-1,j + u_i+1,j) / Delta x^2 +
+                             (u_i,j-1 + u_i,j+1) / Delta y^2 -
+                             2 u_i,j / (Delta x^2 + Delta y^2))
+"""
+from __future__ import print_function
+
+from scipy import ndimage, random
+import numpy as np
+
+from heat import HeatSolver
+
+
+np.set_printoptions(linewidth=120,
+                    formatter={'float_kind': lambda x : '%6.4F' % x})
+
+
+GRID_SHAPE = (11, 11)
+GRID_SPACING = (1., 1.)
+ALPHA = .1
+STOP_TIME = 100.
+
+heat_solver = HeatSolver(shape=GRID_SHAPE, spacing=GRID_SPACING,
+                         alpha=ALPHA)
+heat_solver.temperature[GRID_SHAPE[0] / 2, GRID_SHAPE[1] / 2] = 1.
+
+print('shape: [{ny}, {nx}]'.format(ny=GRID_SHAPE[0], nx=GRID_SHAPE[1]))
+print('spacing: [{dy}, {dx}]'.format(dy=GRID_SPACING[0], dx=GRID_SPACING[1]))
+print('alpha: {alpha}'.format(alpha=ALPHA))
+
+n_steps = int(STOP_TIME / heat_solver.time_step)
+for step in range(n_steps + 1):
+    if step % (n_steps // 5) == 0:
+        print('time: {time}'.format(time=step * heat_solver.time_step))
+        print('temperature:\n{t}'.format(t=heat_solver.temperature))
+
+    heat_solver.solve()
